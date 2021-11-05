36940aca7e08e35a42c2bf6248e32be6736125d8
TheDutchSean <sean@yourfutureacademy.nl>
2021-11-05 14:57:42 +0100

    einde 5-11-2021

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import intro from "../views/intro.vue";
 
-const = [
+const routes = [
     {
         path: "/intro",
         name: "intro",
         component: intro,
     }
 ]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+  });
+
+export default router;
--- src/view/intro.vue
+++ src/view/intro.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <header role="banner">
-            <img class="foqusLogo" src="todo" role="figure">
+            <img class="foqusLogo" src="./images/logo-foqus-werkt-466x320" role="figure">
             Content
-            <h3>Succesvol ondernemen is plezierig ondernemen.</h3>
+            <h3 role="heading">Succesvol ondernemen is plezierig ondernemen.</h3>
         </header>
         <main>
             <section role="contentinfo">
                 <h3 role="heading">Heb jij voldoende focus op succes? Test het zelf door 7 vragen te beantwoorden.</h3>
                 <p>{{step}}/7</p>
-                <button class="volgendeBtn" >volgende</button>
+                <button class="volgendeBtn">volgende</button>
             </section>
         </main>
     <div>
