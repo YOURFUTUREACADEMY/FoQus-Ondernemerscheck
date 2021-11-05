a3d4b0ab8e6ae2f06f1eb3ccf72d9aeb1441e100
TheDutchSean <sean@yourfutureacademy.nl>
2021-11-05 12:18:04 +0100

    1ste opzet routing

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+import intro from "../views/intro.vue";
+
+const = [
+    {
+        path: "/intro",
+        name: "intro",
+        component: intro,
+    }
+]
