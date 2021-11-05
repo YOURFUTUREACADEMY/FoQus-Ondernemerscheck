c272f8a29e562cf082afd15c596712b335b5cf04
TheDutchSean <sean@yourfutureacademy.nl>
2021-11-05 12:52:29 +0100

    2de opzet inro.vue

--- src/view/intro.vue
+++ src/view/intro.vue
@@ -1,6 +1,16 @@
 <template>
-    <div id="app">
-ASDASDASF
-
-    </div>
+    <div>
+        <header role="banner">
+            <img class="foqusLogo" src="todo" role="figure">
+            Content
+            <h3>Succesvol ondernemen is plezierig ondernemen.</h3>
+        </header>
+        <main>
+            <section role="contentinfo">
+                <h3 role="heading">Heb jij voldoende focus op succes? Test het zelf door 7 vragen te beantwoorden.</h3>
+                <p>{{step}}/7</p>
+                <button class="volgendeBtn" >volgende</button>
+            </section>
+        </main>
+    <div>
 </template>
