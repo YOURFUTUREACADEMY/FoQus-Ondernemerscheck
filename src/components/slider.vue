9ffd15eb82d0363a9de03e878668aa3c94927bfd
jobvank <job@yourfutureacademy.nl>
2022-06-16 23:21:25 +0200

    20220616 balk

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,39 +1,43 @@
 
 <template>
   <label for="slider">{{ label }}</label>
-  <input
-    v-model.number="value"
-    type="range"
-    
-    id="slider"
-    :min="min"
-    :max="max"
-    :step="step"
-    :style="style"
-    @change="emitValue" 
-  />
+  <div class="outer">
+    <input
+      v-model.number="value"
+      type="range"
+      class="slider"
+      id="slider"
+      :min="min"
+      :max="max"
+      :step="step"
+      :style="style"
+      @change="emitValue" 
+    >    
+  </div> 
 </template>
 <script>
 export default {
   data() {
     return {
-        value: 0     
+        value: 0,
+        style: {
+          
+        }     
     }
   },
   updated() {
-    let sliderElement = document.getElementById('slider');
+    let sliderElement = document.querySelector(".slider::-webkit-slider-thumb")
     console.log(sliderElement)
-    let sliderStyle = window.getComputedStyle(sliderElement);
-    console.log(sliderStyle);
-    console.log(sliderElement.style)
+    // let sliderStyle = window.getComputedStyle(sliderElement);
+    // console.log(sliderStyle);
+    // console.log(sliderElement.style)
     console.log(this.min, this.max, this.step, this.label)
   },
   props: { 
     min: Number, 
     max: Number, 
     step: Number,
-    label: String,
-    style: Object
+    label: String,    
   },
   methods: {
     emitValue() {
@@ -44,3 +48,54 @@ export default {
   emits: ['sliderChange'],   
 }
 </script>
+<style scoped>
+.outer {
+  position:relative;
+  background-color: #000000;
+  height:50px;
+  width:100%;
+}
+
+
+input[type=range] {
+  position:absolute;
+  -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
+  width: 100%; /* Specific width is required for Firefox. */
+  background: transparent; /* Otherwise white in Chrome */
+  border-color: transparent;
+  color: transparent;
+  opacity: 0;
+}
+
+
+input[type=range]:focus {
+  outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+}
+
+input[type=range]::-ms-track {
+  width: 100%;
+  cursor: pointer;
+
+  /* Hides the slider so custom styles can be added */
+  background: transparent; 
+  border-color: transparent;
+  color: transparent;
+}
+
+
+
+input[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  opacity: 1;
+  border: 1px solid #000000;
+  height: 36px;
+  width: 16px;
+  border-radius: 3px;
+  background: #ffffff;
+  cursor: pointer;
+  margin-top: -14px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; /* Add cool effects to your sliders! */
+}
+
+
+</style>
